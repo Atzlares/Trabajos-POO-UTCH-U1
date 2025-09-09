b71c1f81f85e7d07b8c42ba3f1700b6197269abf
Atzlares <158379505+Atzlares@users.noreply.github.com>
2025-09-09 12:22:34 -0600

    Add files via upload

---
+++ LaWeaFome/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        public string nombre;
+        private int edad;
+        static void Main(string[] args)
+        {
+            Persona P1 = new Persona();
+            P1.nombre = "Dafnis";
+            P1.apellido = "Villagran";
+            
+        }
+    }
+}
